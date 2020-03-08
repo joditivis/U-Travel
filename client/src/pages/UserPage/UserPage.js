0fb278739eb5831751fd03442c4a20dc223dbc84
Stacy Holtz <54336612+stacyholtz6@users.noreply.github.com>
2020-03-08 15:10:22 -0600

    added form (#11)

--- client/src/pages/UserPage/UserPage.js
+++ client/src/pages/UserPage/UserPage.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import { Container, Row } from 'reactstrap';
 import PackingList from '../../components/PackingList';
+import AddItemForm from '../../components/AddItemForm';
 
 class UserPage extends Component {
   state = {
     items: [
       {
         id: 1,
-        title: 'Sunglasses'
+        title: 'Sunglasses',
+        complete: false
       },
       {
         id: 2,
-        title: 'Sunscreen'
+        title: 'Sunscreen',
+        complete: 'false'
+      },
+      {
+        id: 3,
+        title: 'Snorkel',
+        complete: 'false'
       }
     ]
   };
@@ -24,9 +32,18 @@ class UserPage extends Component {
     this.setState({ items });
   };
 
+  addItem = item => {
+    item.id = Math.random();
+    let items = [...this.state.items, item];
+    this.setState({
+      items
+    });
+  };
+
   render() {
     return (
       <Container>
+        <AddItemForm addItem={this.addItem} />
         <Row>
           <PackingList items={this.state.items} deleteItem={this.deleteItem} />
         </Row>
