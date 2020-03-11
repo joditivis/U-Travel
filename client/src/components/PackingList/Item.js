836c12ebac2c3c774eebe4dcb7fdfb5d75ddb9b9
Stacy Holtz <54336612+stacyholtz6@users.noreply.github.com>
2020-03-10 20:29:10 -0600

    update to packing-list (#20)

--- client/src/components/PackingList/Item.js
+++ client/src/components/PackingList/Item.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Button, Form, FormGroup } from 'reactstrap';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
 import './style.css';
 
-const Item = ({ item, index, completeItem, removeItem }) => {
+const Item = ({ item, index, completeItem, removeItem  }) => {
   return (
     <Form className="item-form">
       <FormGroup className="item">
-        <Button
-          className="item-button"
+        {/* <Button */}
+        <Input type="checkbox"
+     
+          className="item-btn"
           size="sm"
           onClick={() => completeItem(index)}
         >
-          Packed
-        </Button>
+      
+            </Input>
+        {/* </Button> */}
         <div style={{ textDecoration: item.isCompleted ? 'line-through' : 'none' }}>
           {item.text}
         </div>
