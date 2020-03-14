ac342a8e53e6f6bc3daefaa47d6d3df7ddfb7ea8
Alix Grillo <55517827+alixgrillo@users.noreply.github.com>
2020-03-14 10:56:25 -0600

    Deploy db (#37)
    * tried to incorporate autocomplete
    
    * updated mongodb connection in passport
    
    Co-authored-by: Alix Grillo <alix.grillo2+github@gmail.com>

--- server.js
+++ server.js
@@ -66,5 +66,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/utravel');
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}! ${process.env.MONGODB_URI}`);
 });
