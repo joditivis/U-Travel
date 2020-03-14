80ddcb3dca2c226e67a86b2342deacac19312a16
Alix Grillo <55517827+alixgrillo@users.noreply.github.com>
2020-03-13 19:17:06 -0600

    Last change (#34)
    * tried to incorporate autocomplete
    
    * hopefully this works to deploy
    
    Co-authored-by: Alix Grillo <alix.grillo2+github@gmail.com>

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   //app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname, './client/build', 'index.html'));
   });
