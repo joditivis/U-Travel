ac68045988d4220af5da01a3aa997c9580304d20
Alix Grillo <alix.grillo2+github@gmail.com>
2020-03-21 08:59:05 -0600

    updated hotel search to have autocomplete on city and save hotel info to database

--- client/src/components/HotelSearch/HotelSearchForm.js
+++ client/src/components/HotelSearch/HotelSearchForm.js
@@ -1,14 +1,105 @@
 import React, { Component } from "react";
 import { Button, Form, FormGroup, Label, Input, Row, Col } from "reactstrap";
 import DatePicker from "../DatePicker/DatePicker";
-import './style.css';
+import "./style.css";
+import moment from "moment";
+import Autosuggest from "react-autosuggest";
+
+let cities = [];
+let lastValue = "";
+
+function getMatchingCities(value) {
+  if (value === "") {
+    return [];
+  }
+
+  return cities.filter(city => city.name);
+}
+
+function getSuggestions(value) {
+  if (value === "") {
+    return [];
+  }
+
+  if (value !== lastValue || !value.substring("(")) {
+    cities = [];
+    lastValue = value;
+    callApi(value)
+      .then(res => {
+        res.data.map(city =>
+          cities.push({ name: `${city.name} (${city.iataCode})` })
+        );
+      })
+      .catch(err => console.log(err));
+  } else if (value.substring("(")) {
+  }
+  return cities.filter(city => city.name);
+}
+
+async function callApi(city) {
+  const response = await fetch(`/airport/${city}`);
+  const body = await response.json();
+  if (response.status !== 200) throw Error(body.message);
+
+  return body;
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return <span>{suggestion.name}</span>;
+}
 
 class HotelSearchForm extends Component {
-  state = {
-    city: "",
-    date: "",
-    numAdults: 1,
-    nonStop: false
+  constructor() {
+    super();
+    this.state = {
+      value: "",
+      valueDest: "",
+      suggestions: [],
+      suggestionsDestination: [],
+      isLoading: false,
+      city: "",
+      date: "",
+      numAdults: 1,
+      nonStop: false,
+      startDate: "",
+      endDate: ""
+    };
+    this.lastRequestId = null;
+  }
+
+  loadSuggestions(value) {
+    // Cancel the previous request
+    if (this.lastRequestId !== null) {
+      clearTimeout(this.lastRequestId);
+    }
+
+    this.setState({
+      isLoading: true
+    });
+
+    // Fake request
+    this.lastRequestId = setTimeout(() => {
+      this.setState({
+        isLoading: false,
+        suggestions: getMatchingCities(value)
+      });
+    }, 1000);
+  }
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    });
+    if (newValue.substring("(")) {
+      var city = newValue.substring(newValue.length - 4, newValue.length - 1);
+      this.setState({
+        city: city
+      });
+    }
   };
 
   handleInputChange = event => {
@@ -20,20 +111,49 @@ class HotelSearchForm extends Component {
     });
   };
 
-  // hotelSearch = () => {
-  //   this.callApi(this.state.city)
-  //     .then(res => this.setState({ origin: res.data }))
-  //     // .then(res2 => console.log(this.state.flights))
-  //     .catch(err => console.log(err));
-  // };
+  onSuggestionsFetchRequested = ({ value, reason }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
 
-  // callApi = async city => {
-  //   const response = await fetch(`/hotel/${city}`);
-  //   const body = await response.json();
-  //   if (response.status !== 200) throw Error(body.message);
+    this.loadSuggestions(value);
+  };
 
-  //   return body;
-  // };
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+      suggestionsDestination: []
+    });
+    cities = [];
+  };
+
+  setDate = datePicker => {
+    console.log("dates should be saving");
+    console.log(datePicker);
+    let startDate =
+      datePicker === null
+        ? ""
+        : moment(datePicker.startDate).format("YYYY-MM-DD");
+    let endDate =
+      datePicker === null
+        ? ""
+        : moment(datePicker.endDate).format("YYYY-MM-DD");
+    this.setState({
+      startDate: startDate,
+      endDate: endDate
+    });
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+    console.log(name, value);
+    console.log(this.state);
+  };
 
   handleSubmit = e => {
     e.preventDefault();
@@ -47,14 +167,42 @@ class HotelSearchForm extends Component {
   };
 
   render() {
+    const {
+      value,
+      suggestions,
+    } = this.state;
+    const inputProps = {
+      placeholder: "Hotel City",
+      value,
+      onChange: this.onChange
+    };
+
     return (
       <div>
         <Form className="hotel-form">
           <Row>
             <Col md={12}>
               <FormGroup className="hotel-form-group">
-                <Label className="hotel-form-label" for="hotelSearch">Staying In</Label>
-                <Input
+                <Label className="hotel-form-label" for="hotelSearch">
+                  Staying In
+                </Label>
+                <Autosuggest
+                      id="hotelSearchInput"
+                      suggestions={suggestions}
+                      onSuggestionsFetchRequested={
+                        this.onSuggestionsFetchRequested
+                      }
+                      onSuggestionsClearRequested={
+                        this.onSuggestionsClearRequested
+                      }
+                      getSuggestionValue={getSuggestionValue}
+                      renderSuggestion={renderSuggestion}
+                      inputProps={inputProps}
+                      name="city"
+                      value={this.state.city}
+                      alwaysRenderSuggestions={false}
+                    />
+                {/* <Input
                   type="text"
                   name="city"
                   id="hotelSearchInput"
@@ -62,7 +210,7 @@ class HotelSearchForm extends Component {
                   onInput={this.hotelSearch}
                   onChange={this.handleInputChange}
                   value={this.state.city}
-                />
+                /> */}
               </FormGroup>
             </Col>
           </Row>
@@ -76,7 +224,9 @@ class HotelSearchForm extends Component {
           <Row>
             <Col md={12}>
               <FormGroup>
-                <Label className="hotel-form-label" for="numOfTravelers">Number of Travelers</Label>
+                <Label className="hotel-form-label" for="numOfTravelers">
+                  Number of Travelers
+                </Label>
                 <Input
                   type="text"
                   name="numOfTravelers"
@@ -87,16 +237,17 @@ class HotelSearchForm extends Component {
             </Col>
           </Row>
 
-        <br></br>
+          <br></br>
 
           <p className="hotel-form-label">Check In / Check Out</p>
-          <DatePicker />
+          <DatePicker  setDate={this.setDate} />
 
-        <br></br>
-        <br></br>
-
+          <br></br>
+          <br></br>
         </Form>
-        <Button className="hotel-form-btn" onClick={this.handleSubmit}>Search</Button>
+        <Button className="hotel-form-btn" onClick={this.handleSubmit}>
+          Search
+        </Button>
         <br></br>
       </div>
     );
