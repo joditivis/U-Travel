01cb3a7f89feae94f7112741d0f6e90b1d07da47
Jodi Tivis <joditivis@gmail.com>
2020-03-25 17:18:16 -0600

    deleted second read me, added number 4 to home page

--- client/src/components/HomePage/HomeInstructionsCard.js
+++ client/src/components/HomePage/HomeInstructionsCard.js
@@ -13,7 +13,7 @@ const InstructionsCard = (props) => {
         <p className="instructions">1. Create an account to start saving your plans</p>
         <p className="instructions">2. Search for flights and hotels</p>
         <p className="instructions">3. Save your packing list so you don't forget the most important items</p>
-        <p className="instructions">5. Add all of your trip expenses and let us calculate 
+        <p className="instructions">4. Add all of your trip expenses and let us calculate 
         <br></br>
         how much you need to save to get to where you want to go</p>
         <br></br>
